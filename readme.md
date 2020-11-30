4b32afc92a047d3969afa94b0367abe2c0bcd5a9
ragnarr18 <ragnarr18@skel.ru.is>
2020-11-30 14:49:18 +0000

    idea for splitting up and dividing the project for now

---
+++ readme.md
@@ -0,0 +1,26 @@
+# theToddler
+the Toddler, a react-native project in app development
+
+rules of engagement
+  to run program: cd /Contactor/ and run npm start
+  to run eslint:  npm run lint
+
+contributers:
+  arnarp18:  Arnar Snær Pálsson
+  bjarnib18: Bjarni Berg Björgvinsson
+  ragnarr18: Ragnar Geir Ragnarsson
+
+feats:
+
+
+extra feats:
+
+
+
+uppskiptun verkefna:
+     rgr:  navigation og listi yfir contacts + create contact
+   arnar:  "a user should be able to search for contacts by their name"
+  bjarni:  contact detail screen, modify information
+
+     all:  File-system setup, extras
+            extrax: import contacts, add image with camera or photo import, user can use it to call someone
---
+++ src/views/Main/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import BoardsList from '../../components/BoardsList';
+import BoardsToolbar from '../../components/BoardsToolbar';
+import data from '../../resources/data.json';
+import AddBoard from '../../components/AddBoard';
+import EditBoard from '../../components/EditBoard';
+import deleteBoards from '../../services/deleteBoards';
+
+class Boards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAddModalOpen: false, isEditModalOpen: false, selectedItems: [], currentName: '', currentThumbnail: '',
+    };
+  }
+
+  getNameAndThumbnail(id) {
+    let board = null;
+    for (let i = 0; i < data.boards.length; i += 1) {
+      if (data.boards[i].id === id) {
+        board = data.boards[i];
+        this.setState({ currentName: board.name, currentThumbnail: board.thumbnailPhoto });
+        break;
+      }
+    }
+  }
+
+  selectOrUnSelect(id) {
+    const { selectedItems } = this.state;
+    const index = selectedItems.indexOf(id);
+    if (index === -1) {
+      selectedItems.push(id);
+      this.getNameAndThumbnail(id);
+      return;
+    }
+    selectedItems.splice(index, 1);
+  }
+
+  deleteSelected() {
+    const { selectedItems } = this.state;
+    deleteBoards(selectedItems);
+    this.setState({ selectedItems: [] });
+  }
+
+  render() {
+    const {
+      isAddModalOpen, isEditModalOpen, currentName, currentThumbnail, selectedItems,
+    } = this.state;
+    const { navigation } = this.props;
+    return (
+      <View>
+        <BoardsList
+          boardsItems={data.boards}
+          navigation={navigation}
+          setSelected={(id) => this.selectOrUnSelect(id)}
+        />
+        <BoardsToolbar
+          onAdd={() => this.setState({ isAddModalOpen: true, selectedItems })}
+          onDelete={() => this.deleteSelected()}
+          onEdit={() => this.setState({ isEditModalOpen: true, selectedItems })}
+        />
+        <AddBoard
+          isOpen={isAddModalOpen}
+          closeModel={() => this.setState({ isAddModalOpen: false })}
+        />
+        {selectedItems.length === 1
+        && (
+        // this.getNameAndThumbnail(selectedItems[0]),
+        <EditBoard
+          isOpen={isEditModalOpen}
+          closeModel={() => this.setState({ isEditModalOpen: false })}
+          id={selectedItems[0]}
+          prevName={currentName}
+          prevThumbnail={currentThumbnail}
+        />
+        )}
+      </View>
+    );
+  }
+}
+Boards.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default Boards;
