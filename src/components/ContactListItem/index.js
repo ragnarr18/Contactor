04723a7abb09f11f68768681446a0aeec9a60213
Bjabbjo <42970264+Bjabbjo@users.noreply.github.com>
2020-11-30 17:45:04 +0000

    Default value set for phone number

--- src/components/ContactListItem/index.js
+++ src/components/ContactListItem/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { Collapse, CollapseHeader, CollapseBody } from 'accordion-collapse-react-native';
+import styles from './styles';
 
 class ContactListItem extends React.Component {
   constructor(props) {
@@ -13,7 +14,7 @@ class ContactListItem extends React.Component {
     // const { item } = this.props;
     const { name, phone } = this.props;
     return (
-      <Collapse>
+      <Collapse style={styles.contactContainer}>
         <CollapseHeader>
           <Text>
             {name}
@@ -42,7 +43,7 @@ ContactListItem.propTypes = {
 }
 */
 ContactListItem.defaultProps = {
-  phone: PropTypes.string = '',
+  phone: PropTypes.string = '\t> missing phone number',
 }
 
 export default ContactListItem;
