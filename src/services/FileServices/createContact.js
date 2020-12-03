e993a12ea0587810360840daf162e1645fef12fa
ragnarr18 <ragnarr18@skel.ru.is>
2020-12-03 18:41:28 +0000

    uuid

--- src/services/FileServices/createContact.js
+++ src/services/FileServices/createContact.js
@@ -1,4 +1,5 @@
 import * as FileSystem from 'expo-file-system';
+import { v4 as uuidv4 } from 'uuid';
 import data from '../../resources/USERS.json';
 import defaultProfilePicture from '../../resources/PROFILE_PIC.json';
 
@@ -15,7 +16,9 @@ async function createContact(contact) {
   await setupDirectory();
   const newContact = { name: contact.name, phone: contact.phone, image: contact.image };
   const newName = contact.name.replace(/\s/g, '').toLowerCase();
-  const fileName = `${contactsDirectory}/${newName}${contact.phone}.json`;
+  // const fileName = `${contactsDirectory}/${newName}${contact.phone}.json`;
+  const id = uuidv4();
+  const fileName = `${contactsDirectory}/{id}.json`;
   // console.log(newStr.toLowerCase());
   // console.log(`${contactsDirectory}/${newName}${populus[i].phone}.json`);
   // console.log(contact.image);
@@ -26,7 +29,8 @@ async function createContact(contact) {
   const fileInfo = await FileSystem.getInfoAsync(`file://${fileName}`);
   console.log('file created: ', fileInfo.exists);
 
-  data.users.push({ name: contact.name, fileName: `${newName}${contact.phone}.json` });
+  // data.users.push({ name: contact.name, fileName: `${newName}${contact.phone}.json` });
+  data.users.push({ name: contact.name, fileName: `${id}.json` });
   return fileInfo.exists;
 }
 
